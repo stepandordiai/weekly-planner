a8a9f1c7535e7cca53647ded6542e0f1ef6f73b6
Stepan Dordiai <stepandordiai@gmail.com>
2025-12-15 16:21:57 +0100

    ...

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -121,7 +121,7 @@ const Home = () => {
 
 	// const usersDayNow = userData;
 
-	const usertt = userData.slice(-6);
+	// const usertt = userData.slice(-6);
 
 	useEffect(() => {
 		if (!usersData?.length) return;
@@ -190,9 +190,11 @@ const Home = () => {
 				<div className="home-users-container">
 					{inputData.people.map((person, i) => (
 						<div className="home-user-container" key={person.name}>
-							<div style={{ display: "flex", justifyContent: "space-between" }}>
-								<button
-									onClick={() => handleDropdonwVisible(i)}
+							<button
+								className={`btn ${dropdownVisible[i] ? "btn--active" : ""}`}
+								onClick={() => handleDropdonwVisible(i)}
+							>
+								<div
 									style={{
 										display: "flex",
 										justifyContent: "center",
@@ -211,19 +213,19 @@ const Home = () => {
 										<path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1" />
 									</svg>
 									<p style={{ fontSize: "1.2rem" }}>{person.name}</p>
-								</button>
+								</div>
 								<p>
 									{userData.length > 0
 										? userData[0].updatedAt.toString().slice(11, 16)
 										: ""}
 								</p>
-							</div>
+							</button>
 							<div
 								className={`dd-container ${
 									dropdownVisible[i] ? "dd-container--visible" : ""
 								}`}
 							>
-								<div style={{ overflow: "hidden" }}>
+								<div style={{ overflow: "hidden", padding: "0 20px" }}>
 									{weekDataModified.map((day, i) => {
 										return <p key={i}>{day}</p>;
 									})}
